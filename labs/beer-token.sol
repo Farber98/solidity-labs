eac0e8bf14963ae8580bc4137d3904d02541c7de
Farber98 <juantuc98@gmail.com>
2022-11-09 16:50:10 -0300

    implemented ERC20 beer token.

---
+++ labs/beer-token.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/*  
+    REQUIREMENTS:
+
+    * We can give users tokens for beers
+    * The user can spend the beer token in his own name, or give it to someone else.
+    * Beer tokens get burned when the user gets his beer and event is emitted. 
+
+*/
+
+
+contract BeerToken is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    event BeerPurchased(address indexed receiver, address indexed buyer);
+
+
+    constructor() ERC20("BeerToken", "BEER") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function buyOneBeer() public {
+        _burn(_msgSender(), 1);
+        emit BeerPurchased(_msgSender(), _msgSender());
+    }
+
+    function buyOneBeerFrom(address account) public {
+        _spendAllowance(account, _msgSender(), 1);
+        _burn(account, 1);
+        emit BeerPurchased(_msgSender(), account);
+    }
+}
