8413dc86e6b24ba5f5b4878996e1560b440a6af9
Farber98 <juantuc98@gmail.com>
2022-11-11 14:48:18 -0300

    truffle deployment.

--- nft/truffle/truffle-config.js
+++ nft/truffle/truffle-config.js
@@ -45,7 +45,8 @@
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
-
+const fs = require("fs");
+const etherscanKey = fs.readFileSync(".etherscan").toString().trim();
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -87,13 +88,12 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
@@ -121,6 +121,11 @@ module.exports = {
       //  evmVersion: "byzantium"
       // }
     },
+  },
+
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: etherscanKey,
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
